d5359c228190e242f988f0519d82d38d9bd5e4f9
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-09-08 15:22:37 -1000

    passed first test

--- vault.js
+++ vault.js
@@ -1,4 +1,18 @@
 'use strict';
 module.exports = function() {
-  
+
+  const vault = (function() {
+    let key;
+    let value;
+    return {
+      getValue : function(k){
+        if(k === undefined) {
+          return null;
+        }
+      },
+      setValue : function(k, v){}
+    };
+  })();
+
+  return vault;
 };
